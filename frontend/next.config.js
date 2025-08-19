9d617c50d8170cbe16a3e5551bbd099fc85fcb89
Aahil Kazi <43545020+Apex-94@users.noreply.github.com>
2025-08-19 21:12:32 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  experimental: { appDir: true },
+  output: 'export',          // enable static export
+  images: { unoptimized: true } // Next/Image in static mode
 };
-
 module.exports = nextConfig;
